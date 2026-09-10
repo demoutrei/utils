9f22edd2a295a6c092fb51ecb863ce740d639df2
demoutrei <works.demoutrei@gmail.com>
2026-09-11 02:17:55 +0800

    [INITIAL]

---
+++ source/conf.py
@@ -0,0 +1,51 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "demoutrei's utilities"
+copyright = '2026, demoutrei'
+author = 'demoutrei'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+  "shibuya",
+  "sphinx_contributors",
+  "sphinx_design",
+  "sphinx_tabs.tabs"
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_context = {
+  "source_type": "github",
+  "source_user": "demoutrei",
+  "source_repo": "guides",
+  "source_version": "main",
+  "source_docs_path": "/source/"
+}
+html_favicon = "_static/demoutrei.png"
+html_theme = 'shibuya'
+html_theme_options = {
+  "accent_color": "green",
+  "color_mode": "dark",
+  "globaltoc_expand_depth": 1,
+  "toctree_collapse": True,
+  "toctree_maxdepth": 5,
+  "show_ai_links": False,
+  "og_image_url": "https://cdn.discordapp.com/embed/avatars/2.png",
+  "discussion_url": "https://github.com/demoutrei/utils/discussions"
+}
+html_static_path = ['_static']
+html_css_files = [ "custom.css" ]
---
+++ source/index.rst
@@ -0,0 +1,15 @@
+.. demoutrei's utilities documentation master file, created by
+   sphinx-quickstart on Fri Sep 11 02:09:17 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+demoutrei's utilities
+=====================
+
+Nothing to see here yet... :eyes:
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
